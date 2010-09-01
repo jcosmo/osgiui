cb4b993abb125b4c64b32af53fd8c4371b42adc0
Peter Donald <peter@realityforge.org>
2010-09-01 10:41:42 +1000

    Annotate class with @Component so it will be processed by iPojo

--- framework/src/main/java/osgiui/framework/MainFrame.java
+++ framework/src/main/java/osgiui/framework/MainFrame.java
@@ -2,10 +2,12 @@ package osgiui.framework;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.handlers.jmx.Config;
 import org.apache.felix.ipojo.handlers.jmx.Method;
 
 @Config
+@Component
 public class MainFrame
   extends JFrame
 {
